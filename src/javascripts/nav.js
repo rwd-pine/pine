f3c6ac6db9a3b08a8aa4077160b0e2ef614adab7
Martin Stanek <info@martinstanek.cz>
2013-10-31 12:38:17 +0100

    New effect - hover fade for desktop

--- src/javascripts/nav.js
+++ src/javascripts/nav.js
@@ -1,52 +1,36 @@
 //
-// Nav: Navigation behavior
+// Responsive navigation module
+//
 // --------------------------------
 
 (function ($, window, undefined) { "use strict";
 
   /**
     Provides the base for Responsive navigation module.
-
-    @module Nav
   **/
   var Nav = (function() {
 
     var version = '0.0.1',
 
-
     Nav = {};
+
     /**
       Stores the state of current view
-
-      @property isDesktop
-      @type Boolean
     **/
     Nav.isDesktop = null;
 
     /**
       References Submenu module.
-
-      @submodule Nav.Submenu
     **/
     Nav.Submenu = $.fn.submenu.Module || {};
 
     /**
       Root navigation element.
-
-      @property Nav.element
-      @type jQuery Element
-      @default null
     **/
     Nav.element = null;
 
-    // TODO: mozna prepsat na privatni promennou
-
     /**
       Default configuration for navigation module.
-
-      @attribute Nav.defaults
-      @readOnly
-      @type boolean
     **/
     Nav.defaults = {
       jsBreakpoint:       '600px',
@@ -58,33 +42,21 @@
 
     /**
       Navigation options that override defaults
-
-      @attribute Nav.options
-      @type Object
-      @default null
     **/
     Nav.options = null;
 
     /**
       List of all available transitions. Transitions are loaded as plugins.
-
-      @attribute Nav.transitions
-      @type Object
     **/
     Nav.transitions = {};
 
     /**
       Applied transition, which depends on the current view (dektop or mobile)
-
-      @attribute Nav.activeTransition
-      @type Object
     **/
     Nav.activeTransition = {};
 
     /**
       Initialize all properties of Nav Module and sets up event listeners
-
-      @method Nav.init
     **/
     Nav.init = function(element, options) {
       this.options = $.extend({}, this.defaults, options);
@@ -92,7 +64,7 @@
       this.isDesktop = window.matchMedia('(min-width: ' + this.options.jsBreakpoint + ')').matches;
 
       // Set initial transition
-      this.isDesktop ? this.setTransition(this.options.transitionDesktop) : this.setTransition(this.options.transitionMobile)
+      this.isDesktop ? this.setActiveTransition(this.options.transitionDesktop) : this.setActiveTransition(this.options.transitionMobile)
 
       // Initialize Submenus
       this.element.find('li').has('ul').addClass('has-submenu')
@@ -101,14 +73,12 @@
       // Default behavior, submenu is triggered on click
       $(document).on('click.submenu', this.options.toggle, this, this.Submenu.toggle)
 
-      // setup API
+      // Setup listeners
       $(window).on('load resize', $.proxy(this.api, this))
     };
 
     /**
-      Navigation module API method assigns appropriate listeners based on conditions.
-
-      @method Nav.api
+      Navigation module API method handles switch between views.
     **/
     Nav.api = function (e) {
       var media = this.checkMedia(e)
@@ -130,10 +100,7 @@
     };
 
     /**
-      Checks current view if it satisfies switch condition. If no switch occured, it returns null.
-
-      @method Nav.checkMedia
-      @return Boolean or null
+      Checks current view if it satisfies switch condition. If no switch occurs, it returns null.
     **/
     Nav.checkMedia = function (e) {
       var condition = window.matchMedia('(min-width: ' + this.options.jsBreakpoint + ')').matches
@@ -149,26 +116,21 @@
 
 
     /**
-      TODO
-
-      @method Nav.switchView
+      Switches active transition when leaving one view and entering another.
     **/
     Nav.switchView = function (isDesktop) {
-      var t = this.getTransition(isDesktop)
+      var t = this.getTransitionName(isDesktop)
 
       this.element
-        .removeClass(this.getTransition(!isDesktop))
+        .removeClass(this.getTransitionName(!isDesktop))
         .addClass(t)
 
-      this.setTransition(t)
+      this.setActiveTransition(t)
     };
 
     // TODO: abstrahovat eventy, nemusi to byt mousenter
     /**
       Event handler which is fired after keyboard input (tab).
-
-      @event Nav.focus
-      @param {Object} Event Object
     **/
     Nav.focus = function (e) {
       // Check if the focused element is part of some Submenu
@@ -190,31 +152,20 @@
 
     /**
       Setter for transitions. New transition is added to transitions collection.
-
-      @event Nav.registerTransition
-      @param {String}   name  Transition name
-      @param {Object}   obj   Transition definition
     **/
-    Nav.setTransition = function (name) {
+    Nav.setActiveTransition = function (name) {
       this.activeTransition = this.transitions[name] || false
     };
 
     /**
-      Getter for transitions.
-
-      @method Nav.getTransition
-      @param {Boolean}   isDesktop
+      Getter for transition.
     **/
-    Nav.getTransition = function (isDesktop) {
+    Nav.getTransitionName = function (isDesktop) {
       return isDesktop ? this.options.transitionDesktop : this.options.transitionMobile
     };
 
     /**
-      Setter for transitions. New transitions is added to transitions collection.
-
-      @event Nav.registerTransition
-      @param {String}   name  Transition name
-      @param {Object}   obj   Transition definition
+      Adds new transition to the collection.
     **/
     Nav.registerTransition = function (name, obj) {
       this.transitions[name] = obj
