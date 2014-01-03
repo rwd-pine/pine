7e1bd5a69e7382fa3b1f2f6ed0261f2c9ec861d0
Martin Stanek <info@martinstanek.cz>
2014-01-03 11:32:35 +0100

    Fixing right-to-left for Zepto

--- src/javascripts/fx/right-to-left.js
+++ src/javascripts/fx/right-to-left.js
@@ -15,19 +15,23 @@ Pine.Navbar.registerTransition('fx-right-to-left', {
       // Enter mobile view
       $submenu.each(function(){
         $(this).find('ul').first()
-          .prepend($('<li class="pine-back"><a href="#">' + $(this).find('> a').text() + '</a></li>'))
+          .prepend($('<li class="pine-back"><a href="#">' + $(this).find('a').first().text() + '</a></li>'))
       })
 
       $(document).on('click', '.pine-back', $.proxy(Pine.Submenu.toggle, this))
 
       $element.find('ul').css('width', $(window).width())
       $(window).on('resize', resizeSubmenu)
+
+      $.log('ENTER small display view')
     }
     else {
       // Leave mobile view
       $element.find('ul').removeAttr('style')
       $submenu.find('li.pine-back').remove()
-      $(window).off('resize')
+      $(window).off('resize', resizeSubmenu)
+
+      $.log('LEAVE small display view')
     }
   },
 
