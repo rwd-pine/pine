52905d1490369da19dfd4fdc7096402185a3455d
Martin Stanek <info@martinstanek.cz>
2014-04-02 14:05:48 +0200

    More security checks

--- move_docs.sh
+++ move_docs.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-SRC_DIR="docs"
+SRC_DIR="./docs/"
 
 # Check if target folder exists
 if [ ! -d "$SRC_DIR" ]; then
+  echo "Target directory does note exist"
   exit 1
 fi
 
-# List content of SRC_DIR
-CONTENT=`ls -l $SRC_DIR | awk '{print $9}'`
+# Check if there are uncomitted changes
+if ! [ "$(git status --porcelain 2>/dev/null)" == "" ]; then
+  echo "There are uncomitted changes, cannot move files"
+  exit 1
+fi
 
 # Switch to Github Pages Branch
 git checkout gh-pages
 
-# Loop through the directories and files
-# for ITEM in $CONTENT
-# do
-#   # Copy all necessary files and folders to gh-pages branch
-#   git checkout master -- ${SRC_DIR}/${ITEM}
-# done
-git checkout master -- ${SRC_DIR}
+# Copy directory from master branch
+git checkout master -- $SRC_DIR
+
+# Loop through the directories and files and move them level up
+# mv -fv $SRC_DIR* ./
+rsync -a $SRC_DIR* ./
+
+# Remove SRC_DIR
+rm -r $SRC_DIR
